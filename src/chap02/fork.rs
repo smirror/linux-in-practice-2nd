39d6ed0a4a2c48f3c5ad8c673fc391bb3eaa51c0
libra <23025996+smirror@users.noreply.github.com>
2023-02-27 00:03:36 +0900

    fix fork

--- src/chap02/fork.rs
+++ src/chap02/fork.rs
@@ -1,17 +1,20 @@
 #!/usr/bin/env rust-script
 // cargo-deps: nix
 
-use nix::unistd::{fork, ForkResult, getppid, getpid};
+use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, getppid, getpid}};
+use std::process::exit;
 
 fn main() {
     match unsafe{fork()} {
         Ok(ForkResult::Parent { child, .. }) => {
             println!("Main pid={}, forked a child pid={}", getpid(), child);
+            waitpid(child,None).unwrap();
         }
         Ok(ForkResult::Child) => {
             println!("Child pid={}, PPID is {}", getpid(), getppid());
+            exit(0)
         }
-        Err(_) => println!("Fork failed"),
+        Err(_) => panic!("Fork failed"),
     }
 
 }
